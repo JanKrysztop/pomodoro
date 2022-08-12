d412d13854cfef4d0578a0773af6f2abfbfc3a2b
Jan Krysztop <jankrysztop@gmail.com>
2022-08-12 16:16:09 +0200

    Timer switch added

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -4,6 +4,9 @@ export const Clock = () => {
   const [minutes, setMinutes] = useState(25);
   const [seconds, setSeconds] = useState(0);
   const [countdown, setCountdown] = useState(false);
+  const [view, setView] = useState({
+    currentView: 'pomodoro',
+  });
 
   const timerMinutes = minutes < 10 ? `0${minutes}` : minutes;
   const timerSeconds = seconds < 10 ? `0${seconds}` : seconds;
@@ -25,6 +28,50 @@ export const Clock = () => {
     }, 1000);
   }
 
+  const switchView = (view) => {
+    switch (view) {
+      case 'pomodoro':
+        setMinutes(25);
+        setSeconds(0);
+        return (
+          <>
+            {timerMinutes}:{timerSeconds}
+          </>
+        );
+      case 'shortBreak':
+        setMinutes(5);
+        setSeconds(0);
+        return (
+          <>
+            {timerMinutes}:{timerSeconds}
+          </>
+        );
+      case 'longBreak':
+        setMinutes(15);
+        setSeconds(0);
+        return (
+          <>
+            {timerMinutes}:{timerSeconds}
+          </>
+        );
+      default:
+        return null;
+    }
+  };
+
+  const pomodoroView = () => {
+    setMinutes(25);
+    setSeconds(0);
+  };
+  const shortBreakView = () => {
+    setMinutes(5);
+    setSeconds(0);
+  };
+  const longBreakView = () => {
+    setMinutes(15);
+    setSeconds(0);
+  };
+
   const handleClick = () => {
     setCountdown(!countdown);
   };
@@ -33,9 +80,15 @@ export const Clock = () => {
     <div className="bg-[#e9d5d4] m-auto flex justify-center align-middle max-w-[620px]">
       <div className="max-w-[480px] w-full bg-[#ee7c78] pt-5 pb-6 rounded-md">
         <div className="flex justify-center align-middle">
-          <button className="btn-clock">Pomodoro</button>
-          <button className="btn-clock">Short Break</button>
-          <button className="btn-clock">Long Break</button>
+          <button className="btn-clock" onClick={() => pomodoroView()}>
+            Pomodoro
+          </button>
+          <button className="btn-clock" onClick={() => shortBreakView()}>
+            Short Break
+          </button>
+          <button className="btn-clock" onClick={() => longBreakView()}>
+            Long Break
+          </button>
         </div>
         <div className="text-9xl font-bold mt-5 text-white text-center">
           {timerMinutes}:{timerSeconds}
